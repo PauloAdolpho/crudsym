1832a4b9ca80b4dabbe789ff2a31e4256a371cdd
Paulo Adolpho <64550903+PauloAdolpho@users.noreply.github.com>
2020-09-26 15:14:30 -0300

    crud

---
+++ migrations/Version20200926153624.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200926153624 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE client (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(100) NOT NULL, cpf VARCHAR(15) NOT NULL, rg VARCHAR(25) NOT NULL, data_de_nascimento DATE NOT NULL, logradouro VARCHAR(255) NOT NULL, complemento VARCHAR(50) NOT NULL, bairro VARCHAR(100) NOT NULL, nome VARCHAR(100) NOT NULL, telefone VARCHAR(15) NOT NULL, email VARCHAR(100) NOT NULL, endereco VARCHAR(250) NOT NULL, idade INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE client');
+    }
+}
---
+++ src/Entity/Client.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ClientRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ClientRepository::class)
+ */
+class Client
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $nome;
+
+    /**
+     * @ORM\Column(type="string", length=15)
+     */
+    private $telefone;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $endereco;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $idade;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNome(): ?string
+    {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    public function getTelefone(): ?string
+    {
+        return $this->telefone;
+    }
+
+    public function setTelefone(string $telefone): self
+    {
+        $this->telefone = $telefone;
+
+        return $this;
+    }
+
+    public function getEndereco(): ?string
+    {
+        return $this->endereco;
+    }
+
+    public function setEndereco(string $endereco): self
+    {
+        $this->endereco = $endereco;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getIdade(): ?int
+    {
+        return $this->idade;
+    }
+
+    public function setIdade(int $idade): self
+    {
+        $this->idade = $idade;
+
+        return $this;
+    }
+}
